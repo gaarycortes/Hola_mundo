3044f0783b669881a30caf8fa1cbd8ded0696ac8
David Cortes <davidcortes@Davids-MacBook-Pro.local>
2017-08-23 14:30:30 -0500

    esto es una prueba

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hola
+        <h1>Esto es un ejemplo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
